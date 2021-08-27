a9a8fb53686d1618bb2c1b63cb9e57658d086df3
Michael Bailey <michaelk.t.bailey@gmail.com>
2021-08-27 16:20:33 +1000

    update tests

--- csum_test.py
+++ csum_test.py
@@ -1,14 +1,17 @@
 import pytest
 from TR_CSUM_Sector import stellarHex
 
-def test_checkObjectPresent():
-    hex = stellarHex()
-    assert hex.objectPresent == False
+@pytest.fixture
+def occupied_hex():
+    ''' Returns a stellarHex object with System Presence set to True'''
+    return stellarHex(True)
 
-def test_determineObjectType():
-    hex = stellarHex()
-    hex.determineObjectType()
+def test_checkObjectPresent(occupied_hex):
+     assert occupied_hex.objectPresent == True
+
+def test_determineObjectType(occupied_hex):
+    occupied_hex.determineObjectType()
 
     expected = ["Star", "Brown Dwarf", "Rogue Planet", "Rogue Gas Giant", "Neutron Star", "Nebula", "Black Hole"]
 
-    assert hex.objectType[0] in expected
+    assert occupied_hex.objectType[0] in expected
