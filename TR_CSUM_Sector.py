ebef651d90d3ba7b382075d62e0bc34eb95d82ef
Michael Bailey <michaelk.t.bailey@gmail.com>
2021-08-27 15:36:43 +1000

    rebuild test framework

--- TR_CSUM_Sector.py
+++ TR_CSUM_Sector.py
@@ -5,29 +5,37 @@ import TR_Support
 
 class stellarHex:
 
-    stars = []
-
     def __init__(self, *args):
         if not args: self.objectPresent = False
         else: self.objectPresent = args[0]
-        
+        self.objectType = []
+      
     
-    def checkSystemPresence(self, subsectorType):
+    def checkObjectPresent(self, subsectorType):
         if subsectorType in TR_Constants.CSUM_DENSITY_MAP:
             x = TR_Support.D100Roll()
             y = TR_Constants.CSUM_DENSITY_MAP[subsectorType]
-            if x <= y: returnBoolean = True
-            else: returnBoolean = False
-            return returnBoolean
-        else:
-            print("ERROR:  Invalid Subsector Type")
-            exit(8)
+            if x <= y: self.objectPresent = True
+            else: self.objectPresent = False
+
+    def determineObjectType(self):
+        x = TR_Support.D100Roll()
+        if x <= 80: self.objectType.append("Star")
+        elif x <= 88: self.objectType.append("Brown Dwarf")
+        elif x <= 94: self.objectType.append("Rogue Planet")
+        elif x <= 97: self.objectType.append("Rogue Gas Giant")
+        elif x <= 98: self.objectType.append("Neutron Star")
+        elif x <= 99: self.objectType.append("Nebula")
+        else: self.objectType.append("Black Hole")
 
             
 
-    
-thisHex = stellarHex(False)
+my_stellarHex = stellarHex(True)
+my_stellarHex.determineObjectType()
 
-print(thisHex.checkSystemPresence("SCATTERED"))
+print("end")
+# thisHex = stellarHex(False)
+
+# print(thisHex.checkSystemPresence("SCATTERED"))
 
 
--- csum_test.py
+++ csum_test.py
@@ -1,21 +1,14 @@
 import pytest
 from TR_CSUM_Sector import stellarHex
 
-@pytest.fixture
-def my_stellarHex():
-    ''' returns a hex with no contents'''
-    return stellarHex()
+def test_checkObjectPresent():
+    hex = stellarHex()
+    assert hex.objectPresent == False
 
-@pytest.mark.parametrize("systemPresence", [
-    (False),
-    (True),
-    ])
+def test_determineObjectType():
+    hex = stellarHex()
+    hex.determineObjectType()
 
-def test_systemPresence(systemPresence):
-    my_stellarHex = stellarHex(systemPresence)
-    assert my_stellarHex.objectPresent == systemPresence
+    expected = ["Star", "Brown Dwarf", "Rogue Planet", "Rogue Gas Giant", "Neutron Star", "Nebula", "Black Hole"]
 
-
-
-
-
+    assert hex.objectType[0] in expected
